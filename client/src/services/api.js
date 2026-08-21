42c72a385cc0a00252299547ea4ed87c937f900e
aditibhakat2 <bhakataditi2@gmail.com>
2026-08-21 15:13:09 +0530

    Fix API base path

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://careersync-3956.onrender.com';
+const API_BASE_URL = 'https://careersync-3956.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
